b56b8b08f03bea01cf781062ddea87100be88c38
Diego Reis <reisdiego144@gmail.com>
2025-07-16 01:50:18 -0300

    First try with Sled
    The results for a single API (outside docker) was absurdly great, unfortunately Sled doesn't support multiprocess access, which basically makes it unusable for rinha. I'll try to find out a way to workaround it, but certainly I'll use an in-process key-value db

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,8 @@ use axum::{
     response::IntoResponse,
     routing::{get, post},
 };
-use moka::future::Cache;
+use chrono::{DateTime, Utc};
 use provider::handler::ProviderHandler;
-use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode, SqliteSynchronous};
-use sqlx::{Pool, Sqlite};
-use sqlx::{Row, SqlitePool};
 use types::{GlobalSummary, PaymentDTO, Summary};
 
 mod provider;
@@ -26,112 +23,68 @@ mod types;
 #[derive(Clone)]
 struct AppState {
     pub handler_sender: async_channel::Sender<PaymentDTO>,
-    pub summary_cache: Arc<Cache<String, GlobalSummary>>,
 }
+
+// (Default, Fallback)
+type TreePair = (sled::Tree, sled::Tree);
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    const NUM_WORKERS: usize = 5;
+    let num_workers: usize = env::var("NUM_WORKERS")
+        .unwrap_or("5".to_string())
+        .parse()
+        .unwrap();
 
-    match connect_db().await {
-        Ok(pool) => {
-            // Initialize one handler per worker
-            let handler = ProviderHandler::new(pool.clone()).await?;
+    let database_url: String = env::var("DATABASE_URL")
+        .unwrap_or("app_db".to_string())
+        .parse()
+        .unwrap();
 
-            let (handler_sender, handler_receiver) = async_channel::unbounded::<PaymentDTO>();
+    let db = sled::open(database_url)?;
 
-            for _ in 0..NUM_WORKERS {
-                spawn_worker(handler_receiver.clone(), handler.clone()).await;
-            }
+    let default_tree = db.open_tree("default")?;
+    let fallback_tree = db.open_tree("fallback")?;
 
-            // HTTP router
-            let app = Router::new()
-                .route("/payments-summary", get(get_payments_summary))
-                .route("/payments", post(exec_payment))
-                .route("/purge-payments", post(purge_payments))
-                .layer(Extension(pool))
-                .with_state(AppState {
-                    handler_sender,
-                    summary_cache: Arc::new(Cache::new(100)),
-                });
+    // Initialize one handler per worker
+    let handler = ProviderHandler::new(default_tree.clone(), fallback_tree.clone()).await?;
 
-            let listener = tokio::net::TcpListener::bind("0.0.0.0:9999").await?;
-            axum::serve(listener, app).await?;
-            return Ok(());
-        }
+    let (handler_sender, handler_receiver) = async_channel::unbounded::<PaymentDTO>();
 
-        Err(err) => {
-            println!("Something went wrong with the db, entering in sleep mode for debugging...");
-            println!("Error: {}", err);
-            sleep(Duration::from_secs(100000000000));
-        }
+    for _ in 0..num_workers {
+        spawn_worker(handler_receiver.clone(), handler.clone()).await;
     }
+
+    // HTTP router
+    let app = Router::new()
+        .route("/payments-summary", get(get_payments_summary))
+        .route("/payments", post(exec_payment))
+        .route("/purge-payments", post(purge_payments))
+        .layer(Extension((default_tree, fallback_tree)))
+        .with_state(AppState { handler_sender });
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:9999").await?;
+    axum::serve(listener, app).await?;
 
     Ok(())
 }
 
 async fn get_payments_summary(
     Query(params): Query<HashMap<String, String>>,
-    Extension(pool): Extension<Pool<Sqlite>>,
-    State(state): State<AppState>,
+    Extension((default_tree, fallback_tree)): Extension<TreePair>,
 ) -> impl IntoResponse {
+    // We just assume the strings are valid
     let from = params
         .get("from")
         .map(String::as_str)
-        .unwrap_or("0000-01-01T00:00:00Z")
-        .to_string();
+        .unwrap_or("0000-01-01T00:00:00Z");
 
     let to = params
         .get("to")
         .map(String::as_str)
-        .unwrap_or("9999-12-31T23:59:59Z")
-        .to_string();
+        .unwrap_or("9999-12-31T23:59:59Z");
 
-    let cache_key = format!("{from}:{to}");
-
-    if let Some(summary) = state.summary_cache.get(&cache_key).await {
-        return (StatusCode::OK, Json(summary));
-    }
-
-    let result = sqlx::query(
-        "
-        SELECT
-            is_default,
-            COUNT(*) AS totalRequests,
-            SUM(amount) AS totalAmount
-        FROM payments
-        WHERE timestamp >= ? AND timestamp <= ?
-        GROUP BY is_default;",
-    )
-    .bind(&from)
-    .bind(&to)
-    .fetch_all(&pool)
-    .await
-    .unwrap();
-
-    let mut default = Summary {
-        total_requests: 0,
-        total_amount: 0.0,
-    };
-
-    let mut fallback = Summary {
-        total_requests: 0,
-        total_amount: 0.0,
-    };
-
-    for row in result {
-        let is_default: u8 = row.get(0);
-        let count: u64 = row.get(1);
-        let sum: f64 = row.get(2);
-
-        if is_default == 1 {
-            default.total_requests = count;
-            default.total_amount = sum;
-        } else {
-            fallback.total_requests = count;
-            fallback.total_amount = sum;
-        }
-    }
+    let default = Summary::from_iter(default_tree.range(from..=to));
+    let fallback = Summary::from_iter(fallback_tree.range(from..=to));
 
     (StatusCode::OK, Json(GlobalSummary { default, fallback }))
 }
@@ -140,19 +93,21 @@ async fn exec_payment(
     State(app_state): State<AppState>,
     Json(payload): Json<PaymentDTO>,
 ) -> impl IntoResponse {
-    if let Err(_) = app_state.handler_sender.send(payload).await {
-        return StatusCode::INTERNAL_SERVER_ERROR;
-    }
-
-    StatusCode::OK
-}
-
-pub async fn purge_payments(Extension(pool): Extension<Pool<Sqlite>>) -> impl IntoResponse {
-    sqlx::query("DELETE FROM payments")
-        .execute(&pool)
+    app_state
+        .handler_sender
+        .send(payload)
         .await
         .map(|_| StatusCode::OK)
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+pub async fn purge_payments(
+    Extension((default, fallback)): Extension<TreePair>,
+) -> impl IntoResponse {
+    match (default.clear(), fallback.clear()) {
+        (Ok(_), Ok(_)) => StatusCode::OK,
+        _ => StatusCode::INTERNAL_SERVER_ERROR,
+    }
 }
 
 pub async fn spawn_worker(rx: async_channel::Receiver<PaymentDTO>, handler: ProviderHandler) {
@@ -163,45 +118,4 @@ pub async fn spawn_worker(rx: async_channel::Receiver<PaymentDTO>, handler: Prov
             }
         }
     });
-}
-
-pub async fn connect_db() -> anyhow::Result<Pool<Sqlite>> {
-    let db_url = env::var("DATABASE_URL").unwrap_or("data/app.db".to_string());
-    let cache_size = env::var("CACHE_SIZE").unwrap_or("-4000".to_string());
-    let mmap_size = env::var("MMAP_SIZE").unwrap_or("5000000".to_string());
-    let busy_timeout = env::var("BUSY_TIMEOUT").unwrap_or("10".to_string());
-
-    let conn_opts = SqliteConnectOptions::new()
-        .filename(&db_url)
-        .create_if_missing(true)
-        .journal_mode(SqliteJournalMode::Wal)
-        .synchronous(SqliteSynchronous::Off) // We just have to survive for 5 min :)
-        .busy_timeout(Duration::from_secs(busy_timeout.parse()?))
-        .pragma("temp_store", "MEMORY")
-        .pragma("mmap_size", mmap_size)
-        .pragma("cache_size", cache_size)
-        .pragma("optimize", "0x10002");
-
-    let pool = SqlitePool::connect_with(conn_opts).await?;
-
-    sqlx::query(
-        "CREATE TABLE IF NOT EXISTS payments (
-            correlation_id TEXT PRIMARY KEY,
-            is_default INTEGER NOT NULL,
-            amount REAL NOT NULL,
-            timestamp TEXT NOT NULL
-        ) WITHOUT ROWID;
-
-        CREATE INDEX IF NOT EXISTS idx_payments_timestamp
-        ON payments (timestamp);
-
-        CREATE INDEX IF NOT EXISTS idx_payments_provider
-        ON payments (is_default, timestamp);
-
-        ANALYZE payments;",
-    )
-    .execute(&pool)
-    .await?;
-
-    Ok(pool)
 }
